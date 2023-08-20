1c73c4ad828532943998da983ab8f0ad3c3218a1
Limin Wu <u7534100@anu.edu.au>
2023-08-20 12:17:09 +1000

    Add project files.

---
+++ FIT5032_MyCodeSnippet/Controllers/HomeController.cs
@@ -0,0 +1,39 @@
+﻿using FIT5032_MyCodeSnippet.HelloWorld;
+using FIT5032_MyCodeSnippet.Models.Exercise;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace FIT5032_MyCodeSnippet.Controllers
+{
+    public class HomeController : Controller
+    {
+        public ActionResult Index()
+        {
+            return View();
+        }
+
+        public ActionResult About()
+        {
+            ViewBag.Message = "Your application description page.";
+
+            Hello hello = new Hello();
+            // I assigned the ViewBag.Message to the result
+            ViewBag.Message = hello.GetHello();
+
+
+
+            return View();
+        }
+
+        public ActionResult Contact()
+        {
+            ViewBag.Message = "Your contact page.";
+
+            return View();
+        }
+    }
+}
